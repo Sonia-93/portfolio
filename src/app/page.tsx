991ffecf11edfdbb0cd68055a52e96acab7884be
Wateta <shimirwasonia5@gmail.com>
2026-09-19 21:09:36 +0200

    news

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import IntersectionSection from "@/components/IntersectionSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <IntersectionSection />
     </main>
   );
 }
